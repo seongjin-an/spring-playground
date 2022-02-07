2d15eb72a6f4920fb872eb7250009346baed5b25
seongjin-an <seongjin4us@gmail.com>
2022-02-08 02:22:05 +0900

    jpa

--- space/src/main/java/com/black/space/service/BookService.java
+++ space/src/main/java/com/black/space/service/BookService.java
@@ -6,6 +6,7 @@ import com.black.space.repository.AuthorRepository;
 import com.black.space.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -33,6 +34,33 @@ public class BookService {
 
         throw new RuntimeException("RUNTIME EXCEPTION");
 //        throw new Exception("ERROR");
+    }
+
+    /*
+    Isolation
+    defualt
+    READ_UNCOMMITED     //dirty read
+    READ_COMMITED       //postgresql
+    REPEATABLE_READ     //mysql, mariadb
+    SERIALIZABLE
+    아래로 갈수록.. 정합도↑, 속도↓
+
+    1.READ_UNCOMMITED 상태에서 포커스를 테스트로 넘긴 상태에서
+      DB 트랜잭션을 걸고 해당 데이터를 업데이후 커밋한 다음 디버깅하여 dirty read를 확인한다.
+    2.업데이트 로직 추가 후 DB에서 해당 데이터에 대해 트랜잭션을 걸고 커밋을 하지 않고
+      락이 걸리는 것을 확인한다. 이후 커밋, 롤백 두 케이스 모두 확인한다.
+     */
+    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
+    public void get(Long id){
+        System.out.println(">>> " + bookRepository.findById(id));
+        System.out.println(">>> " + bookRepository.findAll());
+
+        System.out.println(">>> " + bookRepository.findById(id));
+        System.out.println(">>> " + bookRepository.findAll());
+
+        Book book = bookRepository.findById(1L).get();
+        book.setName("AAAAAAA");
+        bookRepository.save(book);
     }
 
 }
--- space/src/test/java/com/black/space/service/BookServiceTest.java
+++ space/src/test/java/com/black/space/service/BookServiceTest.java
@@ -1,5 +1,6 @@
 package com.black.space.service;
 
+import com.black.space.domain.Book;
 import com.black.space.repository.AuthorRepository;
 import com.black.space.repository.BookRepository;
 import org.junit.jupiter.api.Test;
@@ -32,5 +33,18 @@ class BookServiceTest {
         //TransactionAspectSupport
         System.out.println("BOOKS: " + bookRepository.findAll());
         System.out.println("AUTHORS: " + authorRepository.findAll());
+    }
+
+
+    @Test
+    void isolationTest(){
+        Book book = new Book();
+        book.setName("isolation");
+
+        bookRepository.save(book);
+
+        bookService.get(1L);
+
+        System.out.println(">>> " + bookRepository.findAll());
     }
 }
