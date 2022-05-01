91c2e7b3448a72e50bcceacfaaa8ad6eca27e3f5
seongjin-an <seongjin4us@gmail.com>
2022-05-01 21:13:56 +0900

    first commit for movie

---
+++ object/src/Bag.java
@@ -0,0 +1,34 @@
+public class Bag {
+    private long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public Bag(long amount) {
+        this(null, amount);
+    }
+
+    public Bag(Invitation invitation, Long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(long amount) {
+        this.amount += amount;
+    }
+}
