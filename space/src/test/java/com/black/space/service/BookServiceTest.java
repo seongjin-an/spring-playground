0fe95106dcb1acf34363ebae72dbd7160f4dfc8c
seongjin-an <seongjin4us@gmail.com>
2022-07-31 12:37:42 +0900

    propagation

--- space/src/test/java/com/black/space/service/BookServiceTest.java
+++ space/src/test/java/com/black/space/service/BookServiceTest.java
@@ -3,6 +3,7 @@ package com.black.space.service;
 import com.black.space.domain.Book;
 import com.black.space.repository.AuthorRepository;
 import com.black.space.repository.BookRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,6 +32,19 @@ class BookServiceTest {
             System.out.println("111");
         }
         //TransactionAspectSupport
+        System.out.println("BOOKS: " + bookRepository.findAll());
+        System.out.println("AUTHORS: " + authorRepository.findAll());
+    }
+
+    @DisplayName("runtime exception이 발생해도 롤백이 안되는 경우")
+    @Test
+    void transactionTest2(){
+        try {
+            bookService.put2();
+            //put2 메서드에 진입하는 순간 빈 내부로 진입된거고 해당 메서드가 다른 메서드를 호출하면 거기에 달린 Transactional이 무시됨
+        }catch(RuntimeException e){
+            System.out.println(">>> " + e.getMessage());
+        }
         System.out.println("BOOKS: " + bookRepository.findAll());
         System.out.println("AUTHORS: " + authorRepository.findAll());
     }
