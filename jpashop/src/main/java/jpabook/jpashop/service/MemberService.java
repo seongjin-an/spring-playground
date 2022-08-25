a0bb338a80de93892d9ead97f1ef2b8652efe5f5
seongjin-an <seongjin4us@gmail.com>
2022-08-25 14:05:22 +0900

    member

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // Transactional 애노테이션이 있어야 LAZY 로딩이라던가 변경이 가능하다.
+public class MemberService {
+//    @Autowired
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);//중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+//    @Transactional(readOnly = true)//true 라고 지정할 시 영속성 컨텍스트를 플러시하지 않고 더티체킹하지 않고
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+//    @Transactional(readOnly = true)
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
