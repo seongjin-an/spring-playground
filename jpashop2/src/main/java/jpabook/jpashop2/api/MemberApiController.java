3b9cb24c32e192188465edacae0ef70e561a9b69
seongjin-an <seongjin4us@gmail.com>
2022-09-04 18:24:34 +0900

    project

--- jpashop2/src/main/java/jpabook/jpashop2/api/MemberApiController.java
+++ jpashop2/src/main/java/jpabook/jpashop2/api/MemberApiController.java
@@ -20,6 +20,24 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+
+        public CreateMemberRequest(String name) {
+            this.name = name;
+        }
+    }
+
     @Data
     static class CreateMemberResponse {
         private Long id;
