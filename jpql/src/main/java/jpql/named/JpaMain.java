0edc22670bb55c58e0525ed7c328dfa85a00260a
seongjin-an <seongjin4us@gmail.com>
2022-09-04 16:53:26 +0900

    네임드

---
+++ jpql/src/main/java/jpql/named/JpaMain.java
@@ -0,0 +1,64 @@
+package jpql.named;
+
+import jpql.Member;
+import jpql.MemberType;
+import jpql.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.changeTeam(teamA);
+            member1.setType(MemberType.ADMIN);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.changeTeam(teamA);
+            member2.setType(MemberType.ADMIN);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원2");
+            member3.changeTeam(teamB);
+            member3.setType(MemberType.ADMIN);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
+                    .setParameter("username", "회원1")
+                    .getResultList();
+            for(Member member : resultList) {
+                System.out.println("member = " + member);
+            }
+
+            tx.commit();
+        } catch (Exception error) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
