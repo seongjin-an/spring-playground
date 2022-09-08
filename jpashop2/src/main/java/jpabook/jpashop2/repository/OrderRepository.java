af237a2df0174b85682e404718aa5cc15e066d60
seongjin-an <seongjin4us@gmail.com>
2022-09-08 15:43:00 +0900

    default_batch_fetch_size 로 성능 최적화

--- jpashop2/src/main/java/jpabook/jpashop2/repository/OrderRepository.java
+++ jpashop2/src/main/java/jpabook/jpashop2/repository/OrderRepository.java
@@ -98,6 +98,13 @@ public class OrderRepository {
         return resultList;
     }
 
+
+    /**
+     * findAllWithItem 과 findAllWithMemberDelivery 은 비교해서 장단점을 파악한다.
+     * findAllWithItem 은 적은 양의 쿼리가 발생한다는 장점은 있으나 다의 초점에 맞춰 데이터 뻥튀기가 발생할 수도 있다.
+     * 반면 findAllWithMemberDelivery 는 약간의 쿼리가 더 발생하지만 최적화된 쿼리만 발생하기에 적은 데이터 전송이 발생한다.
+     * 상황에 따라 트레이드오프하자.
+     */
     public List<Order> findAllWithItem() {
         return em.createQuery(// order 가 같은 ID 값이면, 중복을 제거해준다!!(OneToMany 컬렉션 뻥튀기 줄여줌)
                 "select distinct o from Order o " +
@@ -110,5 +117,18 @@ public class OrderRepository {
                 .setFirstResult(1)// 일대다 관계에서는 페이징처리가 안됨.(HHH000104: firstResult/maxResults specified with collection fetch; applying in memory!)
                 .setMaxResults(100)
                 .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        List<Order> resultList = em.createQuery(
+                "SELECT o from Order o " +
+                        " join fetch o.member m " +
+                        " join fetch o.delivery d",
+                        Order.class
+        )
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+        return resultList;
     }
 }
