adf1e8bcf33424d365c61bc458fd457a6e019158
seongjin-an <seongjin4us@gmail.com>
2022-09-10 02:52:02 +0900

    공통 인터페이스

--- data-jpa/src/main/java/jpa/datajpa/DataJpaApplication.java
+++ data-jpa/src/main/java/jpa/datajpa/DataJpaApplication.java
@@ -2,8 +2,10 @@ package jpa.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "jpa.datajpa.repository")//@SpringBootApplication 부터 하위 패키지는 알아서 스캔함(생략가능)
 public class DataJpaApplication {
 
     public static void main(String[] args) {
--- data-jpa/src/main/java/jpa/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/jpa/datajpa/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package jpa.datajpa.repository;
 import jpa.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsername(String username);
 }
