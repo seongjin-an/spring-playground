260a880b2db47a9bc58a204679224e96abf6d4bf
seongjin-an <seongjin4us@gmail.com>
2022-09-09 14:13:49 +0900

    p6spy

--- data-jpa/src/test/java/jpa/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/jpa/datajpa/repository/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback(false)
 class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
