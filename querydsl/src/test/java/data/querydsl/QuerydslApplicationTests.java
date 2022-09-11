e64b1d002e346724e3dd8ed1d4f5ee94bb8b9548
seongjin-an <seongjin4us@gmail.com>
2022-09-11 16:19:27 +0900

    다시 환경설정 및 검증

--- querydsl/src/test/java/data/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/data/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,41 @@
 package data.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import data.querydsl.entity.Hello;
+import data.querydsl.entity.QHello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void entityTest() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello result = query.selectFrom(qHello).fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
