bff41f6bd9404f7d71a51900aef50ba8e9f7c1fd
seongjin-an <seongjin4us@gmail.com>
2022-09-11 12:40:08 +0900

    Query By Example!!

--- data-jpa/src/test/java/jpa/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/jpa/datajpa/repository/MemberRepositoryTest.java
@@ -7,10 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -401,4 +398,32 @@ class MemberRepositoryTest {
 
         assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void queryByExample() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        //Probe
+        Member member = new Member("m1");//도메인 객체로 검색조건을 만들어 버린다고 생각하자.
+        Team team = new Team("teamA");
+        member.setTeam(team);
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("age"); //primitive type 은 무시해주자
+
+        Example<Member> example = Example.of(member, matcher);
+
+        List<Member> result = memberRepository.findAll(example);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("m1");
+    }//JOIN 이 되기는 하는데 OUTER 조인이 잘 안됨..
 }
