def9b6a07a797b3ab1405fb650fd0795c84c36de
seongjin-an <seongjin4us@gmail.com>
2022-09-12 15:58:12 +0900

    @QueryProjection 이란

---
+++ querydsl/src/test/java/data/querydsl/advanced/QuerydslQueryProjection.java
@@ -0,0 +1,62 @@
+package data.querydsl.advanced;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import data.querydsl.dto.MemberDto;
+import data.querydsl.dto.QMemberDto;
+import data.querydsl.entity.Member;
+import data.querydsl.entity.QMember;
+import data.querydsl.entity.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static data.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class QuerydslQueryProjection { // 3 QueryProjection
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @DisplayName("Querydsl @QueryProjection 이란")
+    @Test
+    public void queryProjectionTest() throws Exception{
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+}
